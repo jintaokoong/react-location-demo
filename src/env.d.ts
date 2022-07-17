e45d3abbc34711326be0b72d83827ff7dfb0cf6f
Jintao Koong <jintaokoong99@gmail.com>
2022-07-17 15:05:08 +0800

    🎉 initial commit

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_API_KEY: string
+  readonly VITE_APP_AUTH_DOMAIN: string
+  readonly VITE_APP_PROJECT_ID: string
+  readonly VITE_APP_STORAGE_BUCKET: string
+  readonly VITE_APP_MESSAGING_SENDER_ID: string
+  readonly VITE_APP_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
